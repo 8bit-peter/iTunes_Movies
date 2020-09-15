769a73e12f692e493bd24086467544fa49286e06
Piotr Jaczewski <pp.jaczewski@gmail.com>
2020-09-15 19:23:28 +0200

    Updates

---
+++ precache-manifest.40f61bd6c3368e6950ed9d07c47a2e89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be1996ad747f8c4e7f36527aed589f9",
+    "url": "/itunes-app/index.html"
+  },
+  {
+    "revision": "b05f0803feaae2fbd4b4",
+    "url": "/itunes-app/static/css/main.1bd5c69a.chunk.css"
+  },
+  {
+    "revision": "16f086d7e788c34615f8",
+    "url": "/itunes-app/static/js/2.8fcf54da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/itunes-app/static/js/2.8fcf54da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b05f0803feaae2fbd4b4",
+    "url": "/itunes-app/static/js/main.b7f44e9e.chunk.js"
+  },
+  {
+    "revision": "3c98f567bda7bdbf05b9",
+    "url": "/itunes-app/static/js/runtime-main.11cb3d59.js"
+  }
+]);

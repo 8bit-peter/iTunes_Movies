c857a71b841d38a786a14d0412a1dad7ffbd98d6
Piotr Jaczewski <pp.jaczewski@gmail.com>
2020-09-15 19:05:11 +0200

    first production build, search working, no item view

---
+++ build/precache-manifest.9003701bd3a454d50f7feb0b0252df47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377a547064e08b2d6d3ab6769c25f840",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b05f0803feaae2fbd4b4",
+    "url": "/static/css/main.1bd5c69a.chunk.css"
+  },
+  {
+    "revision": "16f086d7e788c34615f8",
+    "url": "/static/js/2.8fcf54da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8fcf54da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b05f0803feaae2fbd4b4",
+    "url": "/static/js/main.b7f44e9e.chunk.js"
+  },
+  {
+    "revision": "0067b01267f6d4c59918",
+    "url": "/static/js/runtime-main.98201326.js"
+  }
+]);
